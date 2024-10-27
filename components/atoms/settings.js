041b6f737d94610c018527fd8070fdd75b15cff0
1medi <144403286+1medi@users.noreply.github.com>
2024-10-26 19:16:17 -0700

    adding some stuffffsss

--- components/atoms/settings.js
+++ components/atoms/settings.js
@@ -32,7 +32,7 @@ export default function ModalSimpleUsageShowcase() {
         >
           <LinearGradient
             colors={['#88B5DF', '#FFFFFF']}
-            start={{ x: 0.5, y: 0 }} // Customize gradient direction
+            start={{ x: 0.5, y: 0 }}
             end={{ x: 0.5, y: 1 }}
             style={styles.modalBackground}
           >
@@ -42,27 +42,27 @@ export default function ModalSimpleUsageShowcase() {
               <Text category="h5" style={styles.sectionTitle}>Setting</Text>
 
               <Text style={styles.sectionSubtitle}>Account Setting</Text>
-              <Card style={styles.settingCard}>
-                <Button appearance="ghost">Account Information</Button>
+              <Layout style={styles.settingCard}>
+                <Button style={styles.buttonText} appearance="ghost">Account Information</Button>
                 <Button appearance="ghost">Password Management</Button>
                 <Button appearance="ghost">Notification</Button>
                 <Button appearance="ghost">Appearance</Button>
-              </Card>
+              </Layout>
 
               <Text style={styles.sectionSubtitle}>Help & Support</Text>
-              <Card style={styles.settingCard}>
+              <Layout style={styles.settingCard}>
                 <Button appearance="ghost">FAQ</Button>
                 <Button appearance="ghost">Contact Support</Button>
                 <Button appearance="ghost">App Tutorial</Button>
-              </Card>
+              </Layout>
 
               <Text style={styles.sectionSubtitle}>Other</Text>
-              <Card style={styles.settingCard}>
+              <Layout style={styles.settingCard}>
                 <Button appearance="ghost">Terms of Service</Button>
                 <Button appearance="ghost">Privacy Policy</Button>
-              </Card>
+              </Layout>
 
-              <Button style={{marginBottom:40}} onPress={() => setVisible(false)}>DISMISS</Button>
+              <Button style={{marginBottom:40}} onPress={() => setVisible(false)}>Close</Button>
             </ScrollView>
           </LinearGradient>
         </Modal>
@@ -82,13 +82,16 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     padding: 16,
   },
+  buttonText:{
+    textAlign:'left'
+  },
   modalBackground: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
   modalContent: {
-    width: '90%',
+    width: '100%',
     borderRadius: 20,
     padding: 30,
 
@@ -102,7 +105,7 @@ const styles = StyleSheet.create({
   settingCard: {
     borderRadius: 10,
     marginVertical: 10,
-    padding: 10,
+    padding: 0,
     backgroundColor: '#f0f0f0', 
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 4 },
