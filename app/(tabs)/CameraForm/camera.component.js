deb63803e50ad1cf7f2a6725b23e3e3538897ceb
parnelltse <parnell.tse@mgail.com>
2024-11-05 10:48:31 -0800

    fixing background colors

--- app/(tabs)/CameraForm/camera.component.js
+++ app/(tabs)/CameraForm/camera.component.js
@@ -1,16 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView, ScrollView, View } from 'react-native';
-import { Button, Divider, Layout, TopNavigation, Icon } from '@ui-kitten/components';
-import Settings from '@/components/atoms/settings.js'
-import CardSimple from '@/components/atoms/card';
-import { StyleSheet, Text, Image } from 'react-native';
-import OptionButton from '@/components/atoms/optionButton'
-import DarkModeIcon from '@/components/atoms/darkMode'
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import DetectObject from '@/src/index'
-import HeaderProfile from '@/components/molecules/Header';
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, ScrollView, View } from "react-native";
+import {
+  Button,
+  Divider,
+  Layout,
+  TopNavigation,
+  Icon,
+} from "@ui-kitten/components";
+import Settings from "@/components/atoms/settings.js";
+import CardSimple from "@/components/atoms/card";
+import { StyleSheet, Text, Image } from "react-native";
+import OptionButton from "@/components/atoms/optionButton";
+import DarkModeIcon from "@/components/atoms/darkMode";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import DetectObject from "@/src/index";
+import HeaderProfile from "@/components/molecules/Header";
 
 import {
   useFonts,
@@ -23,15 +29,14 @@ import {
   Inter_700Bold,
   Inter_800ExtraBold,
   Inter_900Black,
-} from '@expo-google-fonts/inter';
-import AppLoading from 'expo-app-loading';
-import { LinearGradient } from 'expo-linear-gradient';
+} from "@expo-google-fonts/inter";
+import AppLoading from "expo-app-loading";
+import { LinearGradient } from "expo-linear-gradient";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export const CameraScreen = ({ navigation }) => {
-
   let [fontsLoaded] = useFonts({
     Inter_100Thin,
     Inter_200ExtraLight,
@@ -45,9 +50,8 @@ export const CameraScreen = ({ navigation }) => {
   });
 
   if (!fontsLoaded) {
-    return <AppLoading />
+    return <AppLoading />;
   }
-
 
   // const [currentDate, setCurrentDate] = useState('')
 
@@ -63,70 +67,75 @@ export const CameraScreen = ({ navigation }) => {
   return (
     <>
       <LinearGradient
-        colors={['#ffff', '#c4d3ff']}
+        colors={["#9FC3E5", "#ffff"]}
         style={styles.gradientContainer}
       >
-
-<SafeAreaView style={styles.homePage}>
-<HeaderProfile/>
-          <ScrollView style={{ marginHorizontal: 5, backgroundColor: 'none' }}>
-            <Layout style={{ backgroundColor: 'none', padding: 20, width: 400 }}>
-              <Text style={styles.headerText}>Hello Chris!{"\n"}Need help <Text style={{ fontFamily: 'Inter_800ExtraBold', color: '#2E8BB7' }}>Simplifying </Text>{"\n"}a form today?</Text>
+        <SafeAreaView style={styles.homePage}>
+          <HeaderProfile />
+          <ScrollView style={{ marginHorizontal: 5, backgroundColor: "none" }}>
+            <Layout
+              style={{ backgroundColor: "none", padding: 20, width: 400 }}
+            >
+              <Text style={styles.headerText}>
+                Hello Chris!{"\n"}Need help{" "}
+                <Text
+                  style={{ fontFamily: "Inter_800ExtraBold", color: "#2E8BB7" }}
+                >
+                  Simplifying{" "}
+                </Text>
+                {"\n"}a form today?
+              </Text>
             </Layout>
-            <Layout style={{paddingBottom: 50, backgroundColor:'rgba(0, 0, 0, 0)'}}>
-          <DetectObject/>
-          </Layout>
+            <Layout
+              style={{ paddingBottom: 50, backgroundColor: "rgba(0, 0, 0, 0)" }}
+            >
+              <DetectObject />
+            </Layout>
           </ScrollView>
-
         </SafeAreaView>
-
       </LinearGradient>
-        
     </>
-
   );
 };
 
-const styles = StyleSheet.create(
-  {
-    homePage: {
-      flex: 1,
-      backgroundColor: 'none'
-    },
-    headerText: {
-      fontSize: 32,
-      fontFamily: 'Inter_400Regular',
-      color: '#08415C'
-    },
-    numberContainer: {
-      flexDirection: 'row', 
-      alignItems: 'flex-end', 
-      margin: 20,
-      backgroundColor: 'none',
-      paddingRight:50
-    },
-    largeNumber: {
-      fontSize: 100, 
-      fontWeight: 'bold', 
-      color: '#6D96B7', 
-      marginRight: 10, 
-    },
-    numberTextContainer: {
-      flexDirection: 'column',
-
-    },
-    subText: {
-      fontSize: 16, 
-      color: '#6D96B7', 
-      marginBottom: 5, 
-      width:100
-    },
-    formsText: {
-      fontSize: 24, 
-      fontWeight: 'bold', 
-      color: '#2A374A', 
-    },
-    gradientContainer: {
-      flex: 1,
-    },
-  })
+const styles = StyleSheet.create({
+  homePage: {
+    flex: 1,
+    backgroundColor: "none",
+  },
+  headerText: {
+    fontSize: 32,
+    fontFamily: "Inter_400Regular",
+    color: "#08415C",
+  },
+  numberContainer: {
+    flexDirection: "row",
+    alignItems: "flex-end",
+    margin: 20,
+    backgroundColor: "none",
+    paddingRight: 50,
+  },
+  largeNumber: {
+    fontSize: 100,
+    fontWeight: "bold",
+    color: "#6D96B7",
+    marginRight: 10,
+  },
+  numberTextContainer: {
+    flexDirection: "column",
+  },
+  subText: {
+    fontSize: 16,
+    color: "#6D96B7",
+    marginBottom: 5,
+    width: 100,
+  },
+  formsText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#2A374A",
+  },
+  gradientContainer: {
+    flex: 1,
+  },
+});
--- app/(tabs)/FormHistory/FormHistory.component.js
+++ app/(tabs)/FormHistory/FormHistory.component.js
@@ -1,15 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView, ScrollView, TouchableOpacity, View, } from 'react-native';
-import { Button, Divider, Layout, TopNavigation, Icon, Card } from '@ui-kitten/components';
-import Settings from '@/components/atoms/settings.js';
-import CardSimple from '@/components/atoms/card';
-import { StyleSheet, Text, Image } from 'react-native';
-import OptionButton from '@/components/atoms/optionButton';
-import DarkModeIcon from '@/components/atoms/darkMode';
-import { Input } from '@ui-kitten/components';
-import HeaderProfile from '@/components/molecules/Header';
-import LibraryButton from '@/components/molecules/FormLibraryButtons';
-import SearchBar from "@/components/molecules/SearchBar"
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, ScrollView, TouchableOpacity, View } from "react-native";
+import {
+  Button,
+  Divider,
+  Layout,
+  TopNavigation,
+  Icon,
+  Card,
+} from "@ui-kitten/components";
+import Settings from "@/components/atoms/settings.js";
+import CardSimple from "@/components/atoms/card";
+import { StyleSheet, Text, Image } from "react-native";
+import OptionButton from "@/components/atoms/optionButton";
+import DarkModeIcon from "@/components/atoms/darkMode";
+import { Input } from "@ui-kitten/components";
+import HeaderProfile from "@/components/molecules/Header";
+import LibraryButton from "@/components/molecules/FormLibraryButtons";
+import SearchBar from "@/components/molecules/SearchBar";
 import {
   useFonts,
   Inter_100Thin,
@@ -21,9 +28,9 @@ import {
   Inter_700Bold,
   Inter_800ExtraBold,
   Inter_900Black,
-} from '@expo-google-fonts/inter';
-import AppLoading from 'expo-app-loading';
-import { LinearGradient } from 'expo-linear-gradient';
+} from "@expo-google-fonts/inter";
+import AppLoading from "expo-app-loading";
+import { LinearGradient } from "expo-linear-gradient";
 
 export const FormHistoryScreen = ({ navigation }) => {
   let [fontsLoaded] = useFonts({
@@ -43,36 +50,29 @@ export const FormHistoryScreen = ({ navigation }) => {
   }
 
   const FilterIcon = (props) => (
-    <Icon 
-      name="options-2-outline" 
-      {...props} 
-      style={styles.filterIcon}
-    />
+    <Icon name="options-2-outline" {...props} style={styles.filterIcon} />
   );
 
   return (
     <>
-      <LinearGradient colors={['#ffffff', '#c4d3ff']} style={{ flex: 1 }}>
+      <LinearGradient colors={["#9FC3E5", "#ffff"]} style={{ flex: 1 }}>
         <SafeAreaView style={styles.homePage}>
+          <HeaderProfile />
 
-<HeaderProfile/>
-
-
-          <ScrollView style={{ marginHorizontal: 5, backgroundColor: 'none' }}>
+          <ScrollView style={{ marginHorizontal: 5, backgroundColor: "none" }}>
             <Layout style={styles.sectionTitle}>
               <Text style={styles.headerText}>My Form History</Text>
             </Layout>
 
-
-<SearchBar
-placeholder="Search My Saved Profiles"
-/>
-
+            <SearchBar placeholder="Search My Saved Profiles" />
 
             <Layout style={styles.cardContainer}>
-            <Text>October 2024</Text>
-            <LibraryButton title='Crusty Toes' subheader='It’s a taxable benefit that replaces 
-part of your income when you retire.' />
+              <Text>October 2024</Text>
+              <LibraryButton
+                title="Crusty Toes"
+                subheader="It’s a taxable benefit that replaces 
+part of your income when you retire."
+              />
             </Layout>
           </ScrollView>
         </SafeAreaView>
@@ -84,12 +84,12 @@ part of your income when you retire.' />
 const styles = StyleSheet.create({
   homePage: {
     flex: 1,
-    backgroundColor: 'rgba(0, 0, 0, 0)',
+    backgroundColor: "rgba(0, 0, 0, 0)",
   },
   profileHeader: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
   },
   profileImage: {
     width: 50,
@@ -98,35 +98,34 @@ const styles = StyleSheet.create({
     marginRight: 15,
   },
   headerTextContainer: {
-    display: 'flex',
+    display: "flex",
   },
   headerName: {
     fontSize: 18,
-    fontFamily: 'Inter_700Bold',
-    color: '#08415C',
+    fontFamily: "Inter_700Bold",
+    color: "#08415C",
   },
   headerDate: {
     fontSize: 14,
-    fontFamily: 'Inter_400Regular',
-    color: '#6b7c93',
+    fontFamily: "Inter_400Regular",
+    color: "#6b7c93",
   },
   sectionTitle: {
     paddingLeft: 20,
     marginTop: 10,
-    backgroundColor:'rgba(0, 0, 0, 0)'
+    backgroundColor: "rgba(0, 0, 0, 0)",
   },
   headerText: {
     fontSize: 32,
-    fontFamily: 'Inter_400Regular',
-    color: '#08415C',
-
+    fontFamily: "Inter_400Regular",
+    color: "#08415C",
   },
   searchSection: {
     padding: 10,
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor:'rgba(0, 0, 0, 0)'
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0)",
   },
   searchInput: {
     borderRadius: 20,
@@ -137,43 +136,43 @@ const styles = StyleSheet.create({
     width: 40,
     height: 40,
     borderRadius: 24,
-    backgroundColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
     padding: 5,
   },
   filterIcon: {
     width: 24,
     height: 24,
-    tintColor: '#08415C',
+    tintColor: "#08415C",
   },
   cardContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
     marginTop: 10,
-    backgroundColor:'rgba(0, 0, 0, 0)'
+    backgroundColor: "rgba(0, 0, 0, 0)",
   },
   formHistoryCard: {
     width: 110,
     height: 140,
-    backgroundColor: 'rgba(8,65,92,0.8)',
+    backgroundColor: "rgba(8,65,92,0.8)",
     borderRadius: 20,
     margin: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   cardImage: {
-    width: '100%',
-    height: '75%',
+    width: "100%",
+    height: "75%",
     marginBottom: 12,
-    resizeMode: 'cover',
+    resizeMode: "cover",
     borderRadius: 20,
   },
   cardText: {
-    textAlign: 'center',
-    color: 'white',
+    textAlign: "center",
+    color: "white",
     marginBottom: 5,
     fontSize: 14,
   },
