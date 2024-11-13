5fb727b54f167d6521190706bac894dcb6825ec4
parnelltse <parnell.tse@gmail.com>
2024-11-13 01:02:11 -0800

    styling home page

--- app/(tabs)/home.component.js
+++ app/(tabs)/home.component.js
@@ -1,31 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { SafeAreaView, ScrollView, View } from "react-native";
-import {
-  Button,
-  Divider,
-  Layout,
-  TopNavigation,
-  Icon,
-} from "@ui-kitten/components";
+import { SafeAreaView, ScrollView, View, StyleSheet, Text, Image } from "react-native";
+import { Button, Layout, Icon } from "@ui-kitten/components";
 import Settings from "@/components/atoms/settings.js";
 import CardSimple from "@/components/atoms/card";
-import { StyleSheet, Text, Image } from "react-native";
 import OptionButton from "@/components/atoms/optionButton";
 import DarkModeIcon from "@/components/atoms/darkMode";
 import HeaderProfile from "@/components/molecules/Header";
-
-import {
-  useFonts,
-  Inter_100Thin,
-  Inter_200ExtraLight,
-  Inter_300Light,
-  Inter_400Regular,
-  Inter_500Medium,
-  Inter_600SemiBold,
-  Inter_700Bold,
-  Inter_800ExtraBold,
-  Inter_900Black,
-} from "@expo-google-fonts/inter";
+import LongCard from "@/components/atoms/longcard";
+import { useFonts, Inter_100Thin, Inter_200ExtraLight, Inter_300Light, Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold, Inter_900Black } from "@expo-google-fonts/inter";
 import AppLoading from "expo-app-loading";
 import { LinearGradient } from "expo-linear-gradient";
 
@@ -45,157 +27,94 @@ export const HomeScreen = ({ navigation }) => {
   if (!fontsLoaded) {
     return <AppLoading />;
   }
+
   const SearchIcon = (props) => <Icon name="search-outline" {...props} />;
-
-  const FileTextIcon = (props) => (
-    <Icon name={"file-text-outline"} {...props} />
-  );
-
+  const FileTextIcon = (props) => <Icon name={"file-text-outline"} {...props} />;
   const UploadIcon = (props) => <Icon name={"upload-outline"} {...props} />;
 
   const navigateDetails = () => {
     navigation.navigate("Details");
   };
 
-  // const [currentDate, setCurrentDate] = useState('')
-
-  // useEffect(() => {
-  //   var date = new Date().getDay()
-  //   var month = new Date().getMonth() + 1
-  //   var year = new Date().getFullYear()
-  //   setCurrentDate(
-  //     date + '/' + month + '/' + year
-  //   )
-  // }, [])
-
   return (
     <>
-      <LinearGradient
-        colors={["#9FC3E5", "#ffff"]}
-        style={styles.gradientContainer}
-      >
+      <LinearGradient colors={["#9FC3E5", "#FFFF"]} style={styles.gradientContainer}>
         <SafeAreaView style={styles.homePage}>
           <HeaderProfile />
 
+          {/* ScrollView with vertical scrolling enabled */}
           <ScrollView
-            style={{
-              backgroundColor: "none",
-              minHeight: "100%",
-              paddingBottom: 30,
-            }}
+            contentContainerStyle={styles.scrollContainer} // Ensures content is properly spaced within ScrollView
+            style={styles.scrollView} // Ensures ScrollView takes full screen height
           >
-            <Layout
-              style={{ backgroundColor: "none", padding: 20, width: 400 }}
-            >
+            <Layout style={styles.headerLayout}>
               <Text style={styles.headerText}>
                 Need help{" "}
-                <Text
-                  style={{ fontFamily: "Inter_800ExtraBold", color: "#2E8BB7" }}
-                >
-
-                  Simplifying{" "}
-                </Text>
+                <Text style={styles.headerBoldText}>Simplifying</Text>{" "}
                 {"\n"}a form today?
               </Text>
             </Layout>
-            <Layout
-              style={{
-                flex: 1,
-                textAlign: "right",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column",
-                padding: 20,
-                backgroundColor: "none",
-                borderBottomColor: "white",
-                borderBottomWidth: 2,
-              }}
-            >
-              <Layout style={{ flexDirection: "row", backgroundColor: "none" }}>
-                <Layout
-                  style={{ flexDirection: "column", backgroundColor: "none" }}
-                >
-                  <OptionButton
-                    title="Browse"
-                    accessory={SearchIcon}
-                    destination="Folder"
-                  />
-                  <Text
-                    style={{
-                      textAlign: "center",
-                      color: "#2E8BB7",
-                      fontFamily: "Inter_300Light ",
-                      fontSize: 16,
-                    }}
-                  >
-                    Browse
-                  </Text>
-                </Layout>
 
-                <Layout
-                  style={{ flexDirection: "column", backgroundColor: "none" }}
-                >
-                  <OptionButton
-                    title="Scan"
-                    accessory={FileTextIcon}
-                    destination="Camera"
-                  />
-                  <Text
-                    style={{
-                      textAlign: "center",
-                      color: "#2E8BB7",
-                      fontFamily: "Inter_300Light",
-                      fontSize: 16,
-                    }}
-                  >
-                    Scan
-                  </Text>
-                </Layout>
+            <Layout style={styles.optionLayout}>
+              <Layout style={styles.optionColumn}>
+                <OptionButton title="Browse" accessory={SearchIcon} destination="Folder" />
+                <Text style={styles.optionText}>Browse</Text>
+              </Layout>
 
-                <Layout
-                  style={{ flexDirection: "column", backgroundColor: "none" }}
-                >
-                  <OptionButton
-                    title="Browse"
-                    accessory={UploadIcon}
-                    destination="Camera"
-                  />
-                  <Text
-                    style={{
-                      textAlign: "center",
-                      color: "#2E8BB7",
-                      fontFamily: "Inter_300Light",
-                      fontSize: 16,
-                    }}
-                  >
-                    Upload
-                  </Text>
-                </Layout>
+              <Layout style={styles.optionColumn}>
+                <OptionButton
+                  title="Scan"
+                  accessory={() => <Image source={require('@/assets/images/icon_scan.png')} style={styles.iconImage} />}
+                  destination="Camera"
+                />
+                <Text style={styles.optionText}>Scan</Text>
+              </Layout>
+
+              <Layout style={styles.optionColumn}>
+                <OptionButton title="Browse" accessory={UploadIcon} destination="Camera" />
+                <Text style={styles.optionText}>Upload</Text>
               </Layout>
             </Layout>
 
+            <Text style={styles.quickAccessText}>Quick Access Forms</Text>
             <Layout style={styles.formGrid}>
-              <CardSimple title="Canadian Pension Plan" />
-              <CardSimple title="Medical Form" />
+              <ScrollView horizontal contentContainerStyle={styles.cardScrollContainer}>
+                <CardSimple title="Canadian Pension Plan" />
+                <CardSimple title="Medical Form" />
+                <CardSimple title="Medication Records" />
+              </ScrollView>
             </Layout>
 
-            <Layout style={styles.recentform}>
-              <Text style={styles.headline}>My Recent Forms</Text>
+            <Layout style={styles.recentFormLayout}>
+              <Text style={styles.headline}>Recent Forms</Text>
               <Layout style={styles.recentContent}>
                 <Layout style={styles.formContainer}>
-                  <CardSimple title="Canadian Pension Plan" />
-                  <CardSimple title="Medical Form" />
+                  <LongCard title="Canadian Pension Plan" />
+                  <LongCard title="Medical Form" />
                 </Layout>
-                <Button
-                  onPress={() => navigation.navigate("Library")}
-                  style={styles.ButtonResent}
-                >
-                  View My Form Library
-                </Button>
+
+                {/* Wrap Button inside a container with gradient */}
+                <View style={styles.gradientButtonContainer}>
+                  <LinearGradient
+                    colors={["#71AFCE", "#5B8399"]} // Light to dark gradient colors
+                    start={[0, 0]} // Gradient starts from the left
+                    end={[1, 0]} // Gradient ends at the right
+                    style={styles.gradientButton}
+                  >
+                    <Button
+                      onPress={() => navigation.navigate("Library")}
+                      style={styles.ButtonResent}
+                      status="basic" // This makes the button transparent inside the gradient
+                    >
+                      View My Forms
+                    </Button>
+                  </LinearGradient>
+                </View>
               </Layout>
+
             </Layout>
 
-            <Layout style={{ paddingBottom: 60 }}></Layout>
+            <Layout style={styles.bottomSpacer} />
           </ScrollView>
         </SafeAreaView>
       </LinearGradient>
@@ -204,105 +123,113 @@ export const HomeScreen = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  formContainer: {
-    display: "flex",
-    flexDirection: "row",
-    margin: "auto",
-    maxHeight: 200,
-    backgroundColor: "none",
-    maxWidth: "100%",
-    gap: 10,
-  },
-  formGrid: {
-    display: "flex",
-    paddingTop: 10,
-    flexDirection: "row",
-    margin: "auto",
-    maxHeight: 200,
-    maxWidth: "100%",
-    backgroundColor: "none",
-    maxWidth: "100%",
-    gap: 10,
-  },
   gradientContainer: {
     flex: 1,
+    backgroundColor: "transparent",
   },
   homePage: {
     flex: 1,
-    backgroundColor: "none",
-    height: "100%",
+    backgroundColor: "transparent",
+  },
+  scrollView: {
+    backgroundColor: "transparent",
+    paddingBottom: 80, // Increased bottom padding to create more space for the button
+  },
+  scrollContainer: {
+    flexGrow: 1, // Ensures ScrollView takes the full screen height if content is small
+    paddingBottom: 100, // Adjust this to make sure you have enough space at the bottom
+  },
+  headerLayout: {
+    backgroundColor: "transparent",
+    padding: 20,
+    paddingBottom: 16,
+    width: 400,
   },
   headerText: {
     fontSize: 32,
     fontFamily: "Inter_400Regular",
     color: "#08415C",
   },
-  numberContainer: {
+  headerBoldText: {
+    fontFamily: "Inter_800ExtraBold",
+    color: "#2E8BB7",
+  },
+  optionLayout: {
     flexDirection: "row",
-    alignItems: "flex-end",
-    margin: 20,
-    backgroundColor: "none",
-    paddingRight: 50,
+    backgroundColor: "transparent",
+    marginBottom: 32,
+    justifyContent: "space-around",
   },
-  largeNumber: {
-    fontSize: 100,
-    fontWeight: "bold",
-    color: "#6D96B7",
-    marginRight: 10,
-  },
-  numberTextContainer: {
+  optionColumn: {
     flexDirection: "column",
+    backgroundColor: "transparent",
+    alignItems: "center",
   },
-  subText: {
-    fontSize: 16,
-    color: "#6D96B7",
-    marginBottom: 5,
-    width: 100,
-  },
-  formsText: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#2A374A",
-  },
-  ButtonResent: {
-    width: "95%",
-    heigh: 60,
-    borderRadius: 32,
-    backgroundColor: "#08415C",
-    fontSize: 20,
+  optionText: {
     textAlign: "center",
-    marginTop: 20,
-    marginLeft: 10,
-    marginRight: 10,
+    color: "#08415C",
+    fontFamily: "Inter_300Light",
+    fontSize: 16,
   },
-  recentform: {
-    width: "100%",
-    height: 420,
-    paddingTop: 30,
-    paddingBottom: 30,
-    marginTop: 40,
-    backgroundColor: "rgba(8, 65, 92, 0.80)",
-    borderTopLeftRadius: 32,
-    borderTopRightRadius: 32,
+  iconImage: {
+    width: 36,
+    height: 36,
+  },
+  quickAccessText: {
+    color: "rgba(8, 65, 92, 0.6)",
+    paddingLeft: 16,
+    fontSize: 16,
+  },
+  formGrid: {
+    paddingTop: 10,
+    backgroundColor: "transparent",
+  },
+  cardScrollContainer: {
+    flexDirection: "row",
+    gap: 8,
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingBottom: 32,
+  },
+  recentFormLayout: {
+    backgroundColor: "transparent",
   },
   recentContent: {
     alignItems: "center",
-    backgroundColor: "none",
+    backgroundColor: "transparent",
     paddingTop: 20,
     width: "100%",
   },
+  formContainer: {
+    display: "flex",
+    flexDirection: "column",
+    margin: "auto",
+    backgroundColor: "transparent",
+    maxWidth: "100%",
+    gap: 8,
+  },
   headline: {
-    color: "#ffffff",
-    fontSize: 24,
-    paddingLeft: 20,
+    color: "rgba(8, 65, 92, 0.6)",
+    paddingLeft: 16,
+    fontSize: 16,
   },
-  gradientTextContainer: {
-    paddingHorizontal: 0,
-    borderRadius: 5,
+  gradientButtonContainer: {
+    width: "100%",
+    alignItems: "center", // Centers the button inside the container
+    marginTop: 32,
   },
-  gradientText: {
-    fontFamily: "Inter_800ExtraBold",
-    fontSize: 32,
-    color: "transparent",
-  }
+  gradientButton: {
+    borderRadius: 24,
+    overflow: "hidden", // Ensures gradient is clipped to the button's border
+  },
+  ButtonResent: {
+    width: 398,
+    height: 54,
+    borderRadius: 24,
+    backgroundColor: "transparent", // Button background is transparent
+    fontSize: 20,
+    textAlign: "center",
+  },
+
 });
+
