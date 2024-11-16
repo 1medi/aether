a090e00b14853ae2a1bd796a12b7d14213bcbe49
Jon Santiaguel <jsantiaguel2@my.bcit.ca>
2024-11-16 02:30:18 -0800

    cleaning up form library and starting over

--- app/(tabs)/FormLibrary/FormLibrary.component.js
+++ app/(tabs)/FormLibrary/FormLibrary.component.js
@@ -1,58 +1,20 @@
 import React, { useState } from "react";
-import {
-  Button,
-  Divider,
-  Layout,
-  TopNavigation,
-  Icon,
-  Input,
-} from "@ui-kitten/components";
-import {
-  StyleSheet,
-  Text,
-  Image,
-  SafeAreaView,
-  ScrollView,
-  View,
-} from "react-native";
+import { Button, Divider, Layout, Icon, Input } from "@ui-kitten/components";
+import { StyleSheet, Text, ScrollView, View } from "react-native";
 import Header from "@/components/header/Header";
-import FormListCard from "@/components/molecules/FormListCard";
-import { FilterButton } from "@/components/atoms/filterButton";
-import ConsoleScreen from "@/components/atoms/ConsoleScreen";
-
-const formsData = [
-  {
-    id: 1,
-    category: "Pension Plan",
-    title: "Canadian Pension Plan",
-    description:
-      "A taxable benefit that replaces part of your income when you retire.",
-  },
-  {
-    id: 2,
-    category: "Disability Plan",
-    title: "Disability Plan",
-    description: "Helps replace income in case of disability.",
-  },
-  {
-    id: 3,
-    category: "Taxes",
-    title: "T4 Statements",
-    description: "Tax statements related to income.",
-  },
-];
+import formsData from "@/app/data/mock_data";
 import {
   useFonts,
   DMSans_400Regular,
   DMSans_500Medium,
   DMSans_700Bold,
 } from "@expo-google-fonts/dm-sans";
-
 import AppLoading from "expo-app-loading";
 import { LinearGradient } from "expo-linear-gradient";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export const FormLibraryScreen = ({ navigation }) => {
-  const [filteredForms, setFilteredForms] = useState(formsData);
+  const [filteredForms, setFilteredForms] = useState(formsData || []);
   let [fontsLoaded] = useFonts({
     DMSans_400Regular,
     DMSans_500Medium,
@@ -63,94 +25,14 @@ export const FormLibraryScreen = ({ navigation }) => {
     return <AppLoading />;
   }
 
-  const navigateDetails = () => {
-    navigation.navigate("Details");
-  };
-
-  const FilterIcon = (props) => (
-    <Icon
-      name="options-2-outline"
-      {...props}
-      style={{ width: 32, height: 32, tint: "white" }}
-    />
-  );
-
-  const handleFilterChange = (index) => {
-    const selectedCategory =
-      index.row === 0 ? "All" : formsData[index.row - 1].category; // Check if "All" is selected
-    setFilteredForms(
-      selectedCategory === "All"
-        ? formsData
-        : formsData.filter((form) => form.category === selectedCategory)
-    );
-  };
-
-  const navigateToCategory = (category) => {
-    navigation.navigate("Library", { category });
-  };
-
   return (
     <>
-      <LinearGradient
-        colors={["#9FC3E5", "#ffff"]}
-        style={styles.gradientContainer}
-      >
-        <SafeAreaView style={styles.homePage}>
-          <Header title={"Form Library"} />
-          <Layout
-            style={{
-              backgroundColor: "none",
-              paddingLeft: 20,
-              marginHorizontal: 5,
-            }}
-          >
-            <Text style={styles.headerText}>Form Library</Text>
-          </Layout>
-          <Layout style={styles.numberContainer}>
-            <Text style={styles.largeNumber}>
-              <Text style={{ opacity: 0.5 }}>0</Text>69
-            </Text>
-
-            <View style={styles.numberTextContainer}>
-              <Text style={styles.subText}>
-                Browse from our current library of
-              </Text>
-              <Text style={styles.formsText}>forms</Text>
-            </View>
-          </Layout>
-
-          <FilterButton onSelect={handleFilterChange} />
-          {/* <Layout style={{ backgroundColor: 'none', paddingLeft: 10, paddingRight: 10, width: '100%', display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-            <Input
-              style={{
-                borderRadius: 20,
-                flex: 1, 
-                marginRight: 10, 
-              }}
-              placeholder='Search for forms...'
-            />
-            <Layout style={styles.iconContainer}>
-            <FilterIcon />
-              </Layout>
-          </Layout> */}
-
-          <ScrollView
-            style={{ marginHorizontal: 8, backgroundColor: "none", margin: 10 }}
-          >
-            <Layout style={{ backgroundColor: "none" }}>
-              {filteredForms.map((form) => (
-                <FormListCard
-                  key={form.id}
-                  title={form.title}
-                  subheader={form.description}
-                  onPress={() => navigateToCategory(form.category)}
-                />
-              ))}
-            </Layout>
-          </ScrollView>
-          <ConsoleScreen />
-        </SafeAreaView>
-      </LinearGradient>
+      <SafeAreaView style={styles.fullPage} edges={["top", "left", "right"]}>
+        <Header title={"Form Library"} />
+        <ScrollView
+          style={{ marginHorizontal: 8, backgroundColor: "none", margin: 10 }}
+        ></ScrollView>
+      </SafeAreaView>
     </>
   );
 };
@@ -220,8 +102,6 @@ const styles = StyleSheet.create({
     margin: 10,
     backgroundColor: "none",
     paddingRight: 50,
-    borderTopWidth: 2,
-    borderColor: "white",
   },
   largeNumber: {
     fontSize: 100,
--- components/header/Header.js
+++ components/header/Header.js
@@ -1,37 +1,51 @@
 import React from "react";
 import { StyleSheet, Text, Image, View } from "react-native";
 import { colors, typography } from "../../css/globals";
+import { Layout } from "@ui-kitten/components";
 
-export default function Header({ title }) {
+export default function Header({ title, hasSearchBar, onSearch }) {
   return (
-    <View style={styles.headerContainer}>
-      <Text style={styles.pageTitle}>{title}</Text>
-      <Image
-        style={styles.profileImage}
-        source={require("@/assets/images/lbj.jpg")}
-      />
-    </View>
+    <Layout style={styles.headerContainer}>
+      <View style={styles.topSection}>
+        <Text style={styles.pageTitle}>{title}</Text>
+        <View style={styles.profileBorder}>
+          <Image
+            style={styles.profileImage}
+            source={require("@/assets/images/lbj.jpg")}
+          />
+        </View>
+      </View>
+      
+    </Layout>
   );
 }
 
 const styles = StyleSheet.create({
   headerContainer: {
+    direction: "column",
     backgroundColor: "transparent",
     paddingHorizontal: 16,
     paddingVertical: 8,
     borderBottomColor: colors.apple.lightStroke,
     borderBottomWidth: 1,
+    width: "100%",
+  },
+  topSection: {
+    backgroundColor: "transparent",
     justifyContent: "space-between",
     flexDirection: "row",
     alignItems: "center",
-    width: "100%",
+  },
+  profileBorder: {
+    padding: 2,
+    borderWidth: 3,
+    borderColor: colors.apple.black,
+    borderRadius: 100,
   },
   profileImage: {
-    width: 40,
-    height: 40,
+    width: 32,
+    height: 32,
     borderRadius: 100,
-    borderWidth: 2,
-    borderColor: colors.apple.black,
   },
   pageTitle: {
     ...typography(true).h1,
