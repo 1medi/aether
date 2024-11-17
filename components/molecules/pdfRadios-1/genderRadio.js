b5e10429064e3634007c2f5e00505f43f1e46431
1medi <144403286+1medi@users.noreply.github.com>
2024-11-17 03:16:08 -0800

    Reapply "Merge branch 'medi2' into lab1"
    This reverts commit 835914ef7defb2d95a3369fc5c9fe0c4d904a709.

---
+++ components/molecules/pdfRadios-1/genderRadio.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { CheckBox } from '@rneui/themed';
+import { View } from "react-native";
+
+
+export default function GenderRadio() {
+  const [selectedIndex, setIndex] = useState(0);
+
+  return (
+    <View style={{ flex: 1, flexDirection: "column", position: "relative"}}>
+        <CheckBox
+          size={6}
+          backgroundColor={"none"}
+          checked={selectedIndex === 0}
+          onPress={() => setIndex(0)}
+          checkedIcon="check-square-o"
+          uncheckedIcon="square-o"
+          containerStyle={{ padding:0, margin: 0}}
+        />
+        <CheckBox
+          size={6}
+          backgroundColor={"none"}
+          checked={selectedIndex === 1}
+          onPress={() => setIndex(1)}
+          checkedIcon="check-square-o"
+          uncheckedIcon="square-o"
+          containerStyle={{ padding:0, margin: 0, top: 1.8}}
+        />
+    </View>
+  );
+}
