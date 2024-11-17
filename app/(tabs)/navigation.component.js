996fcbe67bacaf361f80a79fca02a8940b75f733
Jon Santiaguel <jsantiaguel2@my.bcit.ca>
2024-11-16 18:01:51 -0800

    fixing navigation (real this time, shoutout nathan yee)

--- app/(tabs)/navigation.component.js
+++ app/(tabs)/navigation.component.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createStackNavigator } from "@react-navigation/stack";
 import {
   BottomNavigation,
   BottomNavigationTab,
@@ -18,103 +17,80 @@ import LibraryScreen from "./FormLibrary/library.component";
 import { AccountScreen } from "./Account/Account.component";
 import { colors } from "@/css/globals";
 
-const Tab = createBottomTabNavigator();
-const HomeStack = createStackNavigator();
-const FormLibraryStack = createStackNavigator();
-const MyFilesStack = createStackNavigator();
-const AccountStack = createStackNavigator();
+const { Navigator, Screen } = createBottomTabNavigator();
 
 const BottomTabBar = ({ navigation, state }) => (
-  <Layout style={styles.navShadowContainer}>
-    <BlurView intensity={24} style={styles.navOuterContainer} >
+  <View style={styles.navShadowContainer}>
+    <BlurView intensity={24} style={styles.navOuterContainer}>
       <BottomNavigation
         appearance="noIndicator"
         selectedIndex={state.index}
         onSelect={(index) => navigation.navigate(state.routeNames[index])}
         style={styles.navBarContainer}
-      >
-        {state.routes.map((route, index) => {
-          let iconName;
-          if (route.name === "Home") {
-            iconName = state.index === index ? "home" : "home-outline";
-          } else if (route.name === "FormLibrary") {
-            iconName = state.index === index ? "search" : "search-outline";
-          } else if (route.name === "MyFiles") {
-            iconName = state.index === index ? "book" : "book-outline";
-          } else if (route.name === "Account") {
-            iconName = state.index === index ? "person" : "person-outline";
-          }
-          return (
-            <BottomNavigationTab
-              key={route.key}
-              icon={(props) => (
-                <Icon
-                  name={iconName}
-                  fill={colors.apple.black}
-                  style={[props.style, { width: 24, height: 24 }]}
-                />
-              )}
+        >
+        <BottomNavigationTab
+          icon={(props) => (
+            <Icon
+              {...props}
+              name={state.index === 0 ? "home" : "home-outline"}
+              style={{ width: 24, height: 24 }}
             />
-          );
-        })}
+          )}
+        />
+        <BottomNavigationTab
+          icon={(props) => (
+            <Icon
+              {...props}
+              name={state.index === 1 ? "search" : "search-outline"}
+              style={{ width: 24, height: 24 }}
+            />
+          )}
+        />
+        <BottomNavigationTab
+          icon={(props) => (
+            <Icon
+              {...props}
+              name={state.index === 2 ? "book" : "book-outline"}
+              style={{ width: 24, height: 24 }}
+            />
+          )}
+        />
+        <BottomNavigationTab
+          icon={(props) => (
+            <Icon
+              {...props}
+              name={state.index === 3 ? "person" : "person-outline"}
+              style={{ width: 24, height: 24 }}
+            />
+          )}
+        />
       </BottomNavigation>
     </BlurView>
-  </Layout>
-);
-
-const HomeStackNavigator = () => (
-  <HomeStack.Navigator screenOptions={{ headerShown: false }}>
-    <HomeStack.Screen name="Home" component={HomeScreen} />
-    <HomeStack.Screen name="Library" component={LibraryScreen} />
-    <HomeStack.Screen name="Camera" component={CameraScreen} />
-    <HomeStack.Screen name="PensionPlan" component={AutofilledScreen} />
-  </HomeStack.Navigator>
-);
-
-const FormLibraryStackNavigator = () => (
-  <FormLibraryStack.Navigator screenOptions={{ headerShown: false }}>
-    <FormLibraryStack.Screen name="FormLibrary" component={FormLibraryScreen} />
-    <FormLibraryStack.Screen name="Library" component={LibraryScreen} />
-    <FormLibraryStack.Screen name="Camera" component={CameraScreen} />
-    <FormLibraryStack.Screen name="PensionPlan" component={AutofilledScreen} />
-  </FormLibraryStack.Navigator>
-);
-
-const MyFilesStackNavigator = () => (
-  <MyFilesStack.Navigator screenOptions={{ headerShown: false }}>
-    <MyFilesStack.Screen name="MyFiles" component={MyFilesScreen} />
-    <MyFilesStack.Screen name="Library" component={LibraryScreen} />
-    <MyFilesStack.Screen name="Camera" component={CameraScreen} />
-    <MyFilesStack.Screen name="PensionPlan" component={AutofilledScreen} />
-  </MyFilesStack.Navigator>
-);
-
-const AccountStackNavigator = () => (
-  <AccountStack.Navigator screenOptions={{ headerShown: false }}>
-    <AccountStack.Screen name="Account" component={AccountScreen} />
-    <AccountStack.Screen name="Library" component={LibraryScreen} />
-    <AccountStack.Screen name="Camera" component={CameraScreen} />
-    <AccountStack.Screen name="PensionPlan" component={AutofilledScreen} />
-  </AccountStack.Navigator>
+  </View>
 );
 
 const TabNavigator = () => (
-  <Tab.Navigator
+  <Navigator
     screenOptions={{
       headerShown: false,
     }}
     tabBar={(props) => <BottomTabBar {...props} />}
   >
-    <Tab.Screen name="Home" component={HomeStackNavigator} />
-    <Tab.Screen name="FormLibrary" component={FormLibraryStackNavigator} />
-    <Tab.Screen name="MyFiles" component={MyFilesStackNavigator} />
-    <Tab.Screen name="Account" component={AccountStackNavigator} />
-  </Tab.Navigator>
+    <Screen
+      name="Home"
+      component={HomeScreen}
+      options={{ title: "Aether Home" }}
+    />
+    <Screen name="FormLibrary" component={FormLibraryScreen} />
+    <Screen name="MyFiles" component={MyFilesScreen} />
+    <Screen name="Account" component={AccountScreen} />
+    <Screen name="LibraryScreen" component={LibraryScreen} />
+    <Screen name="Camera" component={CameraScreen} />
+    <Screen name="PensionPlan" component={AutofilledScreen} />
+  </Navigator>
 );
 
-const AppNavigator = () => (
-  <TabNavigator />
-);
+const AppNavigator = () => <TabNavigator />;
 
 const styles = StyleSheet.create({
   navShadowContainer: {
