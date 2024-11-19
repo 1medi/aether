c4c1e482b5bde1a876021ec4b6f2b7f47bf7a07e
Boi San Ly <sanboily.2003@gmail.com>
2024-11-18 23:09:42 -0800

    Adding Dark Mode to Accout page

--- .history/components/atoms/MainFab_20241118225407.js
+++ .history/components/atoms/MainFab_20241118225407.js
@@ -1,17 +1,11 @@
 import React from "react";
 import { TouchableOpacity, StyleSheet } from "react-native";
 import { Icon } from "@ui-kitten/components";
-import { colors, typography } from "@/css/globals";
 
 const MainFab = ({ expanded, onPress }) => {
+
   return (
-    <TouchableOpacity
-      style={[
-        styles.fab,
-        expanded && { opacity: 0.5 }, // Add opacity dynamically when expanded
-      ]}
-      onPress={onPress}
-    >
+    <TouchableOpacity style={expanded ? styles.mainFab: styles.subFab} onPress={onPress}>
       <Icon
         name={expanded ? "close-outline" : "plus-outline"}
         fill="white"
@@ -22,18 +16,24 @@ const MainFab = ({ expanded, onPress }) => {
 };
 
 const styles = StyleSheet.create({
-  fab: {
+  mainFab: {
     width: 80,
     height: 80,
-    borderRadius: 40, // Fixed for a circular button
-    backgroundColor: colors.other.lightBlue ,
+    borderRadius: '50%',
+    backgroundColor: "#71AFCE",
     justifyContent: "center",
     alignItems: "center",
-    elevation: 5, // Android shadow
-    shadowColor: "#000", // iOS shadow
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
+    elevation: 5,
+    opacity: '50%'
+  },
+  subFab: {
+    width: 80,
+    height: 80,
+    borderRadius: '50%',
+    backgroundColor: "#71AFCE",
+    justifyContent: "center",
+    alignItems: "center",
+    elevation: 5,
   },
   fabIcon: {
     width: 30,
@@ -42,7 +42,6 @@ const styles = StyleSheet.create({
 });
 
 export default MainFab;
-
 
 
 
