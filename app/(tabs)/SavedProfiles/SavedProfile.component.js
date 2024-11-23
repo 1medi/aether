9a6876d17f16c8a0ddd1805f1d1bbc7d9c3c9cbe
jasantiaguel <jsantiaguel2@my.bcit.ca>
2024-11-22 18:37:29 -0800

    adjusting back button destination for saved profile view

--- app/(tabs)/SavedProfiles/SavedProfile.component.js
+++ app/(tabs)/SavedProfiles/SavedProfile.component.js
@@ -7,14 +7,13 @@ import {
   TouchableOpacity,
   ScrollView,
 } from "react-native";
-import { Layout, Button, Icon, Toggle, Divider } from "@ui-kitten/components";
+import { Layout, Button, Icon, Divider } from "@ui-kitten/components";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { colors, typography } from "@/css/globals";
 import { useDarkMode } from "../context/DarkModeContext";
-import { color } from "@rneui/base";
 
 export default function SavedProfile({ route, navigation }) {
-  const { isDarkMode, toggleDarkMode } = useDarkMode();
+  const { isDarkMode } = useDarkMode();
   const { profile } = route.params;
 
   const styles = useMemo(() => {
@@ -36,245 +35,245 @@ export default function SavedProfile({ route, navigation }) {
   const { personalInfo, address, emergencyContact } = profile;
 
   return (
-    <>
-      <SafeAreaView style={styles.fullPage} edges={["top", "left", "right"]}>
-        <ScrollView
-          contentContainerStyle={styles.scrollContainer}
-          showsVerticalScrollIndicator={false}
-        >
-          <TouchableOpacity
-            onPress={() => navigation.goBack()}
-            style={styles.backButtonContainer}
-          >
-            <Icon
-              name="arrow-back"
-              style={styles.backIcon}
-              width="24"
-              height="24"
-              fill={colors.apple.black}
-            />
-            <Text style={styles.backButton}>Back</Text>
-          </TouchableOpacity>
+    <SafeAreaView style={styles.fullPage} edges={["top", "left", "right"]}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("MyFiles")}
+        style={styles.backButtonContainer}
+      >
+        <Icon
+          name="arrow-back"
+          style={styles.backIcon}
+          width={24}
+          height={24}
+          fill={colors.apple.black}
+        />
+        <Text style={styles.backButton}>Back</Text>
+      </TouchableOpacity>
+      <ScrollView
+        contentContainerStyle={styles.scrollContainer}
+        showsVerticalScrollIndicator={false}
+      >
+        {/* Profile Picture and Name */}
+        <View style={styles.profileHeader}>
+          <Image source={personalInfo.image} style={styles.profileImage} />
+          <Text style={styles.profileName}>{personalInfo.fullName}</Text>
+          <Text style={styles.profileRole}>
+            {personalInfo.relationshipToUser}
+          </Text>
+        </View>
 
-          {/* Profile Picture and Name */}
-          <View style={styles.profileHeader}>
-            <Image source={personalInfo.image} style={styles.profileImage} />
-            <Text style={styles.profileName}>{personalInfo.fullName}</Text>
-            <Text style={styles.profileRole}>
-              {personalInfo.relationshipToUser}
-            </Text>
-          </View>
-
-          {/* Personal Info Section */}
-          <View style={styles.category}>
-            <Text style={styles.categoryTitle}>Personal Info</Text>
-            <View style={styles.infoItem}>
-              <View style={styles.leftSide}>
-                <Text style={styles.infoText}>{personalInfo.fullName}</Text>
-              </View>
-              <View style={styles.rightSide}>
-                <Icon
-                  name="person-outline"
-                  fill={colors.apple.black}
-                  width="24"
-                  height="24"
-                />
-              </View>
+        {/* Personal Info Section */}
+        <View style={styles.category}>
+          <Text style={styles.categoryTitle}>Personal Info</Text>
+          <View style={styles.infoItem}>
+            <View style={styles.leftSide}>
+              <Text style={styles.infoText}>{personalInfo.fullName}</Text>
             </View>
-            <Divider style={styles.divider} />
-            <View style={styles.infoItem}>
-              <View style={styles.leftSide}>
-                <Text style={styles.infoText}>{personalInfo.phoneNumber}</Text>
-              </View>
-              <View style={styles.rightSide}>
-                <Icon
-                  name="phone-outline"
-                  fill={colors.apple.black}
-                  width="24"
-                  height="24"
-                />
-              </View>
-            </View>
-            <Divider style={styles.divider} />
-            <View style={styles.infoItem}>
-              <View style={styles.leftSide}>
-                <Text style={styles.infoText}>{personalInfo.dateOfBirth}</Text>
-              </View>
-              <View style={styles.rightSide}>
-                <Icon
-                  name="calendar-outline"
-                  fill={colors.apple.black}
-                  width="24"
-                  height="24"
-                />
-              </View>
-            </View>
-            <Divider style={styles.divider} />
-            <View style={styles.infoItem}>
-              <View style={styles.leftSide}>
-                <Text style={styles.infoText}>{personalInfo.gender}</Text>
-              </View>
-              <View style={styles.rightSide}>
-                <Icon
-                  name="arrow-ios-downward-outline"
-                  fill={colors.apple.black}
-                  width="24"
-                  height="24"
-                />
-              </View>
-            </View>
-            <Divider style={styles.divider} />
-            <View style={styles.infoItem}>
-              <View style={styles.leftSide}>
-                <Text style={styles.infoText}>
-                  {personalInfo.relationshipToUser}
-                </Text>
-              </View>
-              <View style={styles.rightSide}>
-                <Icon
-                  name="arrow-ios-downward-outline"
-                  fill={colors.apple.black}
-                  width="24"
-                  height="24"
-                />
-              </View>
+            <View style={styles.rightSide}>
+              <Icon
+                name="person-outline"
+                fill={colors.apple.black}
+                width={24}
+                height={24}
+              />
             </View>
           </View>
-
-          {/* Address Section */}
-          <View style={styles.category}>
-            <Text style={styles.categoryTitle}>Address</Text>
-            <View style={styles.infoItem}>
-              <View style={styles.leftSide}>
-                <Text style={styles.infoText}>{address.streetAddress}</Text>
-              </View>
-              <View style={styles.rightSide}>
-                <Icon
-                  name="home-outline"
-                  fill={colors.apple.black}
-                  width="24"
-                  height="24"
-                />
-              </View>
+          <Divider style={styles.divider} />
+          <View style={styles.infoItem}>
+            <View style={styles.leftSide}>
+              <Text style={styles.infoText}>{personalInfo.phoneNumber}</Text>
             </View>
-            <Divider style={styles.divider} />
-            <View style={styles.infoItem}>
-              <View style={styles.leftSide}>
-                <Text style={styles.infoText}>{address.province}</Text>
-              </View>
-              <View style={styles.rightSide}>
-                <Icon
-                  name="arrow-ios-downward-outline"
-                  fill={colors.apple.black}
-                  width="24"
-                  height="24"
-                />
-              </View>
-            </View>
-                <Divider style={styles.divider} />
-            <View style={styles.infoItem}>
-              <View style={styles.leftSide}>
-                <Text style={styles.infoText}>{address.city}</Text>
-              </View>
-              <View style={styles.rightSide}>
-                <Icon
-                  name="arrow-ios-downward-outline"
-                  fill={colors.apple.black}
-                  width="24"
-                  height="24"
-                />
-              </View>
-            </View>
-            <Divider style={styles.divider} />
-            <View style={styles.infoItem}>
-              <View style={styles.leftSide}>
-                <Text style={styles.infoText}>{address.postalCode}</Text>
-              </View>
-              <View style={styles.rightSide}>
-                <Icon
-                  name="pin-outline"
-                  fill={colors.apple.black}
-                  width="24"
-                  height="24"
-                />
-              </View>
+            <View style={styles.rightSide}>
+              <Icon
+                name="phone-outline"
+                fill={colors.apple.black}
+                width={24}
+                height={24}
+              />
             </View>
           </View>
-
-          {/* Emergency Contact Section */}
-          <View style={styles.category}>
-            <Text style={styles.categoryTitle}>Emergency Contact</Text>
-            <View style={styles.infoItem}>
-              <View style={styles.leftSide}>
-              <Text style={styles.infoText}>{emergencyContact.fullName}</Text>
-              </View>
-              <View style={styles.rightSide}>
-                <Icon
-                  name="people-outline"
-                  fill={colors.apple.black}
-                  width="24"
-                  height="24"
-                />
-              </View>
+          <Divider style={styles.divider} />
+          <View style={styles.infoItem}>
+            <View style={styles.leftSide}>
+              <Text style={styles.infoText}>{personalInfo.dateOfBirth}</Text>
             </View>
-            <Divider style={styles.divider} />
-            <View style={styles.infoItem}>
-              <View style={styles.leftSide}>
+            <View style={styles.rightSide}>
+              <Icon
+                name="calendar-outline"
+                fill={colors.apple.black}
+                width={24}
+                height={24}
+              />
+            </View>
+          </View>
+          <Divider style={styles.divider} />
+          <View style={styles.infoItem}>
+            <View style={styles.leftSide}>
+              <Text style={styles.infoText}>{personalInfo.gender}</Text>
+            </View>
+            <View style={styles.rightSide}>
+              <Icon
+                name="arrow-ios-downward-outline"
+                fill={colors.apple.black}
+                width={24}
+                height={24}
+              />
+            </View>
+          </View>
+          <Divider style={styles.divider} />
+          <View style={styles.infoItem}>
+            <View style={styles.leftSide}>
               <Text style={styles.infoText}>
-                {emergencyContact.phoneNumber}
+                {personalInfo.relationshipToUser}
               </Text>
-              </View>
-              <View style={styles.rightSide}>
-                <Icon
-                  name="phone-outline"
-                  fill={colors.apple.black}
-                  width="24"
-                  height="24"
-                />
-              </View>
             </View>
-            <Divider style={styles.divider} />
-            <View style={styles.infoItem}>
-              <View style={styles.leftSide}>
+            <View style={styles.rightSide}>
+              <Icon
+                name="arrow-ios-downward-outline"
+                fill={colors.apple.black}
+                width={24}
+                height={24}
+              />
+            </View>
+          </View>
+        </View>
+
+        {/* Address Section */}
+        <View style={styles.category}>
+          <Text style={styles.categoryTitle}>Address</Text>
+          <View style={styles.infoItem}>
+            <View style={styles.leftSide}>
+              <Text style={styles.infoText}>{address.streetAddress}</Text>
+            </View>
+            <View style={styles.rightSide}>
+              <Icon
+                name="home-outline"
+                fill={colors.apple.black}
+                width={24}
+                height={24}
+              />
+            </View>
+          </View>
+          <Divider style={styles.divider} />
+          <View style={styles.infoItem}>
+            <View style={styles.leftSide}>
+              <Text style={styles.infoText}>{address.province}</Text>
+            </View>
+            <View style={styles.rightSide}>
+              <Icon
+                name="arrow-ios-downward-outline"
+                fill={colors.apple.black}
+                width={24}
+                height={24}
+              />
+            </View>
+          </View>
+          <Divider style={styles.divider} />
+          <View style={styles.infoItem}>
+            <View style={styles.leftSide}>
+              <Text style={styles.infoText}>{address.city}</Text>
+            </View>
+            <View style={styles.rightSide}>
+              <Icon
+                name="arrow-ios-downward-outline"
+                fill={colors.apple.black}
+                width={24}
+                height={24}
+              />
+            </View>
+          </View>
+          <Divider style={styles.divider} />
+          <View style={styles.infoItem}>
+            <View style={styles.leftSide}>
+              <Text style={styles.infoText}>{address.postalCode}</Text>
+            </View>
+            <View style={styles.rightSide}>
+              <Icon
+                name="pin-outline"
+                fill={colors.apple.black}
+                width={24}
+                height={24}
+              />
+            </View>
+          </View>
+        </View>
+
+        {/* Emergency Contact Section */}
+        <View style={styles.category}>
+          <Text style={styles.categoryTitle}>Emergency Contact</Text>
+          <View style={styles.infoItem}>
+            <View style={styles.leftSide}>
+              <Text style={styles.infoText}>{emergencyContact.fullName}</Text>
+            </View>
+            <View style={styles.rightSide}>
+              <Icon
+                name="people-outline"
+                fill={colors.apple.black}
+                width={24}
+                height={24}
+              />
+            </View>
+          </View>
+          <Divider style={styles.divider} />
+          <View style={styles.infoItem}>
+            <View style={styles.leftSide}>
+              <Text style={styles.infoText}>{emergencyContact.phoneNumber}</Text>
+            </View>
+            <View style={styles.rightSide}>
+              <Icon
+                name="phone-outline"
+                fill={colors.apple.black}
+                width={24}
+                height={24}
+              />
+            </View>
+          </View>
+          <Divider style={styles.divider} />
+          <View style={styles.infoItem}>
+            <View style={styles.leftSide}>
               <Text style={styles.infoText}>{emergencyContact.email}</Text>
-              </View>
-              <View style={styles.rightSide}>
-                <Icon name="email-outline" fill={colors.apple.black} width="24" height="24" />
-                </View>
             </View>
-            <Divider style={styles.divider} />
-            <View style={styles.infoItem}>
-              <View style={styles.leftSide}>
+            <View style={styles.rightSide}>
+              <Icon
+                name="email-outline"
+                fill={colors.apple.black}
+                width={24}
+                height={24}
+              />
+            </View>
+          </View>
+          <Divider style={styles.divider} />
+          <View style={styles.infoItem}>
+            <View style={styles.leftSide}>
               <Text style={styles.infoText}>
                 {emergencyContact.relationshipToProfile}
               </Text>
-              </View>
-              <View style={styles.rightSide}>
-                <Icon
-                  name="arrow-ios-downward-outline"
-                  fill={colors.apple.black}
-                  width="24"
-                  height="24"
-                />
-              </View>
+            </View>
+            <View style={styles.rightSide}>
+              <Icon
+                name="arrow-ios-downward-outline"
+                fill={colors.apple.black}
+                width={24}
+                height={24}
+              />
             </View>
           </View>
+        </View>
 
-          {/* Delete Button */}
-          <TouchableOpacity style={styles.touchContainer}>
-            <Layout style={styles.deleteSection}>
-              <Button
-                status="danger"
-                appearance="filled"
-                style={styles.deleteButton}
-              >
-                Delete Profile
-              </Button>
-            </Layout>
-          </TouchableOpacity>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+        {/* Delete Button */}
+        <TouchableOpacity style={styles.touchContainer}>
+          <Layout style={styles.deleteSection}>
+            <Button
+              status="danger"
+              appearance="filled"
+              style={styles.deleteButton}
+            >
+              Delete Profile
+            </Button>
+          </Layout>
+        </TouchableOpacity>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -295,7 +294,8 @@ const getStyles = (isDarkMode) => ({
     alignItems: "center",
     gap: 8,
     marginHorizontal: 12,
-    // marginBottom: 16,
+    marginTop: 20,
+    marginBottom: 8,
   },
   backButton: {
     ...typography(true).h3Med,
